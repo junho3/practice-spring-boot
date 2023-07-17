3ad010161dec89106d9b1479283a6a53f144037e
Junho <chojunho433@gmail.com>
2023-07-17 20:58:09 +0900

    OrderRepository 상속받도록 변경

--- src/main/kotlin/com/example/demo/infrastructure/persistence/OrderJpaRepository.kt
+++ src/main/kotlin/com/example/demo/infrastructure/persistence/OrderJpaRepository.kt
@@ -1,6 +1,7 @@
 package com.example.demo.infrastructure.persistence
 
 import com.example.demo.core.order.domain.Order
+import com.example.demo.core.order.port.OrderRepository
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface OrderJpaRepository : JpaRepository<Order, Long>
+interface OrderJpaRepository : OrderRepository, JpaRepository<Order, Long>
