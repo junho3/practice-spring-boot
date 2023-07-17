bb248734b539c618eb0d0a8ae47090d17ca674b5
Junho <chojunho433@gmail.com>
2023-07-17 20:26:29 +0900

    h2 database 디펜던시 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,8 @@ dependencies {
     /** JPA */
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
+    runtimeOnly("com.h2database:h2")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
