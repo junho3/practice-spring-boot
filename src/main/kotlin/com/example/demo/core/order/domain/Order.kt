5aa96c21147f3fcbca943a54d64ecc9b18a2d7af
Junho <chojunho433@gmail.com>
2023-07-17 20:51:36 +0900

    주문과 주문상품 OneToMany 관계 설정

--- src/main/kotlin/com/example/demo/core/order/domain/Order.kt
+++ src/main/kotlin/com/example/demo/core/order/domain/Order.kt
@@ -1,10 +1,13 @@
 package com.example.demo.core.order.domain
 
+import javax.persistence.CascadeType
 import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
+import javax.persistence.JoinColumn
+import javax.persistence.OneToMany
 import javax.persistence.Table
 
 @Entity
@@ -17,4 +20,8 @@ class Order(
 
     @Column(name = "name")
     val name: String,
+
+    @OneToMany(cascade=[CascadeType.ALL])
+    @JoinColumn(name = "order_product_id")
+    val products: MutableList<OrderProduct> = mutableListOf(),
 )
