e2a2916df367f8bc1816825917551822f593e413
Junho <chojunho433@gmail.com>
2023-07-19 00:05:37 +0900

    테스트환경 JdbcTemplate Config 추가

---
+++ src/test/kotlin/com/example/demo/JdbcTemplateTestConfig.kt
@@ -0,0 +1,14 @@
+package com.example.demo
+
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
+import org.springframework.jdbc.core.JdbcTemplate
+
+
+@TestConfiguration
+class JdbcTemplateTestConfig {
+    @Bean
+    fun jdbcTemplate(): JdbcTemplate {
+        return JdbcTemplate()
+    }
+}
