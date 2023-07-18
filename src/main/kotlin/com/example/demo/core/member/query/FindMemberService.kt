e70d4b1f148efafb406b38ecf1039c098f345971
Junho <chojunho433@gmail.com>
2023-07-18 22:51:49 +0900

    회원 수를 청크로 구하는 메소드 추가

---
+++ src/main/kotlin/com/example/demo/core/member/query/FindMemberService.kt
@@ -0,0 +1,37 @@
+package com.example.demo.core.member.query
+
+import com.example.demo.core.member.port.MemberRepository
+import mu.KotlinLogging
+import org.springframework.data.domain.PageRequest
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+
+@Transactional(readOnly = true)
+@Service
+class FindMemberService(
+    private val memberRepository: MemberRepository,
+) {
+
+    fun getMemberCountByChunk(chunkSize: Int): Int {
+        var page = 0
+        var count = 0
+
+        do {
+            val pageable = PageRequest.of(page, chunkSize)
+            val memberPages = memberRepository.findAll(pageable)
+
+            memberPages.content.forEach {
+                count++
+                logger.info { "count: $count | name: ${it.name}" }
+            }
+
+
+            page++
+        } while (memberPages.hasNext())
+
+        return count
+    }
+}
+
+private val logger = KotlinLogging.logger {}
