5782cd3eaa4b75bee241326dd395e3f4a32c4d69
feb.ruary <feb.ruary@kakaoinsurecorp.com>
2023-10-12 13:53:12 +0900

    Transactional 람다 함수를 정적으로 사용할 수 있도록 개선

--- src/main/kotlin/com/example/demo/config/persistence/Transactional.kt
+++ src/main/kotlin/com/example/demo/config/persistence/Transactional.kt
@@ -4,9 +4,27 @@ import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
 
 @Component
-class Transactional {
-    @Transactional
-    operator fun <T> invoke(functionInTransaction: () -> T?): T? {
-        return functionInTransaction()
+class Transactional(
+    _transactionalAdvice: TransactionalAdvice
+) {
+
+    init {
+        transactional = _transactionalAdvice
+    }
+
+    companion object {
+        private lateinit var transactional: TransactionalAdvice
+
+        operator fun <T> invoke(function: () -> T): T {
+            return transactional { function() }
+        }
+    }
+
+    @Component
+    class TransactionalAdvice {
+        @Transactional
+        operator fun <T> invoke(function: () -> T): T {
+            return function()
+        }
     }
 }
