611fc6d949a53a3974ad811880f03e304dac80de
feb.ruary <feb.ruary@kakaoinsurecorp.com>
2023-10-18 16:16:35 +0900

    @DataJpaTest에서 @SpringBootTest로 변경함에 따라 불필요한 JdbcTemplate 주입 제거

--- src/test/kotlin/com/example/demo/core/member/SaveMassMemberDataTest.kt
+++ src/test/kotlin/com/example/demo/core/member/SaveMassMemberDataTest.kt
@@ -7,18 +7,15 @@ import com.example.demo.infrastructure.persistence.member.MemberJdbcRepository
 import com.example.demo.infrastructure.persistence.member.MemberJpaRepository
 import io.kotest.core.annotation.DisplayName
 import io.kotest.core.spec.style.FunSpec
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.test.context.ActiveProfiles
 
 @ActiveProfiles("test")
 @SpringBootTest
 @DisplayName("SaveMassMemberDataTest")
 class SaveMassMemberDataTest(
-    private val jdbcTemplate: JdbcTemplate,
     private val memberJpaRepository: MemberJpaRepository,
-    private val memberJdbcRepository: MemberJdbcRepository = MemberJdbcRepository(jdbcTemplate = jdbcTemplate),
+    private val memberJdbcRepository: MemberJdbcRepository,
     private val memberExposedRepository: MemberExposedRepository,
 ) : FunSpec({
 
