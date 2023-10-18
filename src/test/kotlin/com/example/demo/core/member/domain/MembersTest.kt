4a9ab2e7babbea45c32f347b2cfb0d086d99df0d
feb.ruary <feb.ruary@kakaoinsurecorp.com>
2023-10-18 16:22:00 +0900

    Member Jpa 엔티티와 Exposed  엔티티 프로퍼티 검증 테스트 코드 추가

---
+++ src/test/kotlin/com/example/demo/core/member/domain/MembersTest.kt
@@ -0,0 +1,18 @@
+package com.example.demo.core.member.domain
+
+import io.kotest.core.annotation.DisplayName
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+import kotlin.reflect.full.memberProperties
+
+@DisplayName("Members")
+internal class MembersTest : FunSpec({
+    test("Member Jpa와 Exposed의 엔티티 필드는 동일해야 한다.") {
+        val jpaProperties = Member::class.memberProperties.map { it.name }
+        val exposedProperties = Members::class.memberProperties.map { it.name }
+
+        jpaProperties.forEach { property ->
+            exposedProperties.any { it == property } shouldBe true
+        }
+    }
+},)
