6bf38f3f34cdca9ce8c657bc19159f8d364fd772
feb.ruary <feb.ruary@kakaoinsurecorp.com>
2023-10-20 15:01:20 +0900

    ktlint 위반사항 교정

--- src/test/kotlin/com/example/demo/archunit/NamingConventionTest.kt
+++ src/test/kotlin/com/example/demo/archunit/NamingConventionTest.kt
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Service
 
 @AnalyzeClasses(packages = ["com.example.demo"])
 class NamingConventionTest {
-
     @ArchTest
     val servicesShouldBeSuffixed: ArchRule =
         ArchRuleDefinition.classes()
