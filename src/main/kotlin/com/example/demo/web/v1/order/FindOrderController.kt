79192cb468540b4655d6bf79da34a6b3a2ceaa93
feb.ruary <feb.ruary@kakaoinsurecorp.com>
2023-10-30 16:55:49 +0900

    주문 조회 컨트롤러 추가

---
+++ src/main/kotlin/com/example/demo/web/v1/order/FindOrderController.kt
@@ -0,0 +1,20 @@
+package com.example.demo.web.v1.order
+
+import com.example.demo.core.order.service.FindOrderService
+import com.example.demo.web.v1.ApiResponse
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class FindOrderController(
+    private val findOrderService: FindOrderService,
+) {
+    @GetMapping("/v1/orders/{id}")
+    fun findById(@PathVariable("id") id: Long): ApiResponse<FindOrderResponse> {
+        val data = findOrderService.findById(id)
+            .let { FindOrderResponse.from(it) }
+
+        return ApiResponse.success(data)
+    }
+}
