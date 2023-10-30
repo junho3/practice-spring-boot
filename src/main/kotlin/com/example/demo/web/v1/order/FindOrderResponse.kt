854edb32ebd1db2b86b28e5d0b0fe07a288dd251
feb.ruary <feb.ruary@kakaoinsurecorp.com>
2023-10-30 16:56:34 +0900

    N+1 위험성 주석 추가

--- src/main/kotlin/com/example/demo/web/v1/order/FindOrderResponse.kt
+++ src/main/kotlin/com/example/demo/web/v1/order/FindOrderResponse.kt
@@ -29,7 +29,7 @@ data class FindOrderResponse(
             return FindOrderResponse(
                 id = id!!,
                 memberName = memberName,
-                products = products.map { Product.from(it) },
+                products = products.map { Product.from(it) }, // N+1 이슈가 있으므로 운영환경 코드로는 적합하지 않음
             )
         }
     }
