b7423ee8b593cb2a1e2222eebcb137b5b9284ecb
feb.ruary <feb.ruary@kakaoinsurecorp.com>
2023-10-30 16:55:22 +0900

    entity to dto 정적 팩토리 메소드 추가

--- src/main/kotlin/com/example/demo/web/v1/order/FindOrderResponse.kt
+++ src/main/kotlin/com/example/demo/web/v1/order/FindOrderResponse.kt
@@ -1,5 +1,8 @@
 package com.example.demo.web.v1.order
 
+import com.example.demo.core.order.domain.Order
+import com.example.demo.core.order.domain.OrderProduct
+
 data class FindOrderResponse(
     val id: Long,
     val memberName: String,
@@ -9,5 +12,25 @@ data class FindOrderResponse(
         val id: Long,
         val name: String,
         val quantity: Long,
-    )
+    ) {
+        companion object {
+            fun from(product: OrderProduct): Product = with(product) {
+                return Product(
+                    id = id!!,
+                    name = name,
+                    quantity = quantity,
+                )
+            }
+        }
+    }
+
+    companion object {
+        fun from(order: Order): FindOrderResponse = with(order) {
+            return FindOrderResponse(
+                id = id!!,
+                memberName = memberName,
+                products = products.map { Product.from(it) },
+            )
+        }
+    }
 }
