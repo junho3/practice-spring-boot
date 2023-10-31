03a8f2c863c4f2c9bb365fb46a3f7a923fd2d553
feb.ruary <feb.ruary@kakaoinsurecorp.com>
2023-10-31 13:35:30 +0900

    레이어 아키텍처 패키지 참조 검증 피트니스함수 추가

---
+++ src/test/kotlin/com/example/fitness/LayeredArchitectureTest.kt
@@ -0,0 +1,19 @@
+package com.example.fitness
+
+import com.tngtech.archunit.core.importer.ImportOption
+import com.tngtech.archunit.junit.AnalyzeClasses
+import com.tngtech.archunit.junit.ArchTest
+import com.tngtech.archunit.lang.ArchRule
+import com.tngtech.archunit.library.Architectures
+
+@AnalyzeClasses(packages = ["com.example.demo"], importOptions = [ImportOption.DoNotIncludeTests::class])
+class LayeredArchitectureTest {
+    @ArchTest
+    val layerDependenciesAreRespected: ArchRule = Architectures.layeredArchitecture().consideringAllDependencies()
+        .layer("Controllers").definedBy("com.example.demo.web.v1..")
+        .layer("Services").definedBy("com.example.demo.core..service..")
+        .layer("Persistence").definedBy("com.example.demo.infrastructure.persistence..")
+        .whereLayer("Controllers").mayNotBeAccessedByAnyLayer()
+        .whereLayer("Services").mayOnlyBeAccessedByLayers("Controllers")
+        .whereLayer("Persistence").mayOnlyBeAccessedByLayers("Services")
+}
