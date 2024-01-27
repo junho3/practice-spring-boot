82230ad596d3e500d1df42877666764822bc62d3
Junho <chojunho433@gmail.com>
2024-01-27 21:34:06 +0900

    write 커넥션 명시

--- src/main/kotlin/com/example/demo/infrastructure/persistence/member/MemberExposedRepository.kt
+++ src/main/kotlin/com/example/demo/infrastructure/persistence/member/MemberExposedRepository.kt
@@ -1,5 +1,6 @@
 package com.example.demo.infrastructure.persistence.member
 
+import com.example.demo.config.persistence.ExposedDataSourceConfig
 import com.example.demo.core.member.domain.Member
 import com.example.demo.core.member.domain.Members
 import org.jetbrains.exposed.sql.batchInsert
@@ -7,10 +8,12 @@ import org.jetbrains.exposed.sql.transactions.transaction
 import org.springframework.stereotype.Repository
 
 @Repository
-class MemberExposedRepository {
+class MemberExposedRepository(
+    private val exposedDataSourceConfig: ExposedDataSourceConfig,
+) {
     // Jpa 엔티티를 사용하는 것이 마음에 들지 않지만, 우선은 Jpa 엔티티를 파라미터로 받아서 처리
     fun saveAll(members: List<Member>) {
-        transaction {
+        transaction(exposedDataSourceConfig.write()) {
             Members.batchInsert(
                 data = members,
                 ignore = false,
