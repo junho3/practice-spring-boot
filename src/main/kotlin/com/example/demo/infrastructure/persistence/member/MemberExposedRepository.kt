817804b5274e732b7d2fc6feea4ca6b6f1767b3a
feb.ruary <feb.ruary@kakaoinsurecorp.com>
2024-01-29 14:23:10 +0900

    lint 수정

--- src/main/kotlin/com/example/demo/infrastructure/persistence/member/MemberExposedRepository.kt
+++ src/main/kotlin/com/example/demo/infrastructure/persistence/member/MemberExposedRepository.kt
@@ -3,6 +3,5 @@ package com.example.demo.infrastructure.persistence.member
 import com.example.demo.core.member.domain.Member
 
 interface MemberExposedRepository {
-
     fun saveAll(members: List<Member>)
 }
--- src/main/kotlin/com/example/demo/infrastructure/persistence/member/MemberJdbcRepository.kt
+++ src/main/kotlin/com/example/demo/infrastructure/persistence/member/MemberJdbcRepository.kt
@@ -3,6 +3,5 @@ package com.example.demo.infrastructure.persistence.member
 import com.example.demo.core.member.domain.Member
 
 interface MemberJdbcRepository {
-
     fun saveAll(members: List<Member>)
 }
