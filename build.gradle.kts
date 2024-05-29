f49a5cbd003d06a36958d64f1d6d79b26c7c9662
Junho <chojunho433@gmail.com>
2024-05-29 13:09:35 +0900

    kotest 버전 5.7.2 로 최신화

--- build.gradle.kts
+++ build.gradle.kts
@@ -50,8 +50,8 @@ dependencies {
     runtimeOnly("com.h2database:h2")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("io.kotest:kotest-runner-junit5:5.5.5")
-    testImplementation("io.kotest:kotest-assertions-core:5.5.5")
+    testImplementation("io.kotest:kotest-runner-junit5:5.7.2")
+    testImplementation("io.kotest:kotest-assertions-core:5.7.2")
     testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.2")
     testImplementation("com.h2database:h2")
     testImplementation("io.mockk:mockk:1.13.2")
