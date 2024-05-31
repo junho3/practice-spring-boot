8e8a7e066eae2d9d90558064e9b10a6d90d58e72
junho3 <chojunho433@gmail.com>
2024-05-31 16:38:25 +0900

    주문생성 fixtures 개선

--- src/testFixtures/kotlin/com/example/demo/OrderFixtures.kt
+++ src/testFixtures/kotlin/com/example/demo/OrderFixtures.kt
@@ -2,23 +2,29 @@ package com.example.demo
 
 import com.example.demo.core.order.domain.Order
 import com.example.demo.core.order.domain.OrderProduct
+import com.navercorp.fixturemonkey.FixtureMonkey
+import com.navercorp.fixturemonkey.customizer.Values
+import com.navercorp.fixturemonkey.kotlin.KotlinPlugin
+import com.navercorp.fixturemonkey.kotlin.giveMeBuilder
+import com.navercorp.fixturemonkey.kotlin.set
 
 fun createOrder(memberName: String = "어피치"): Order {
-    val order = Order(memberName = memberName)
-    listOf(createOrderProduct(order = order))
+    val order = FixtureMonkey
+        .builder()
+        .plugin(KotlinPlugin())
+        .build()
+        .giveMeBuilder<Order>()
+        .sample()
+
+    (1..10).map {
+        FixtureMonkey.builder()
+            .plugin(KotlinPlugin())
+            .build()
+            .giveMeBuilder<OrderProduct>()
+            .set(OrderProduct::order, Values.just(order))
+            .sample()
+    }
         .forEach { order.addProduct(it) }
 
     return order
-}
-
-fun createOrderProduct(
-    name: String = "카스타드",
-    quantity: Long = 1,
-    order: Order,
-): OrderProduct {
-    return OrderProduct(
-        name = name,
-        quantity = quantity,
-        order = order,
-    )
 }
