fe1881bff7badf18059dc5ed19075998f422138b
junho3 <chojunho433@gmail.com>
2024-05-31 16:58:22 +0900

    Fixtures 데이터를 테스트 코드에서 직접 생성하도록 변경

--- src/test/kotlin/com/example/demo/core/order/service/FindOrderServiceTest.kt
+++ src/test/kotlin/com/example/demo/core/order/service/FindOrderServiceTest.kt
@@ -1,9 +1,10 @@
 package com.example.demo.core.order.service
 
+import com.example.demo.FixturesMonkey
 import com.example.demo.PersistenceDataJpaTest
 import com.example.demo.core.order.domain.Order
-import com.example.demo.createOrder
 import com.example.demo.infrastructure.persistence.order.OrderRepository
+import com.navercorp.fixturemonkey.kotlin.giveMeBuilder
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.annotation.DisplayName
 import io.kotest.core.spec.style.DescribeSpec
@@ -24,8 +25,16 @@ internal class FindOrderServiceTest(
     }
 
     describe("findById 메소드는") {
+
         context("주문 데이터가 존재하면") {
-            orderRepository.save(createOrder())
+
+            beforeTest {
+                val order = FixturesMonkey.fixture()
+                    .giveMeBuilder<Order>()
+                    .sample()
+
+                orderRepository.save(order)
+            }
 
             it("Order 객체를 리턴한다.") {
                 val result = findOrderService.findById(1)
@@ -35,6 +44,7 @@ internal class FindOrderServiceTest(
         }
 
         context("주문 데이터가 존재하지 않으면") {
+
             it("IllegalArgumentException을 던진다.") {
                 val exception = shouldThrow<IllegalArgumentException> { findOrderService.findById(2) }
 
