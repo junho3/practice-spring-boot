0a7f5bec712a12f29efc19d93e1b512be568fd6e
junho3 <chojunho433@gmail.com>
2024-06-05 11:08:27 +0900

    ApiResponse.error 추가

--- src/main/kotlin/com/example/demo/web/v1/ApiResponse.kt
+++ src/main/kotlin/com/example/demo/web/v1/ApiResponse.kt
@@ -1,5 +1,7 @@
 package com.example.demo.web.v1
 
+import com.example.demo.common.exception.ErrorCode
+
 data class ApiResponse<E>(
     val success: Boolean,
     val code: String? = null,
@@ -11,6 +13,24 @@ data class ApiResponse<E>(
             return ApiResponse(
                 success = true,
                 data = result,
+            )
+        }
+
+        fun <E> error(errorCode: ErrorCode): ApiResponse<E> {
+            return ApiResponse(
+                success = false,
+                code = errorCode.code,
+                message = errorCode.message,
+                data = null
+            )
+        }
+
+        fun <E> error(errorCode: ErrorCode, message: String): ApiResponse<E> {
+            return ApiResponse(
+                success = false,
+                code = errorCode.code,
+                message = message,
+                data = null
             )
         }
     }
