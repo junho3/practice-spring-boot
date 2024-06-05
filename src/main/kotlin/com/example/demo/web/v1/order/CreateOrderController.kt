0fa9c9b392d260e3c765dc6cd005483790d94eb8
junho3 <chojunho433@gmail.com>
2024-06-05 09:31:02 +0900

    컨트롤러 추가

---
+++ src/main/kotlin/com/example/demo/web/v1/order/CreateOrderController.kt
@@ -0,0 +1,24 @@
+package com.example.demo.web.v1.order
+
+import com.example.demo.web.v1.ApiResponse
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class CreateOrderController {
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/v1/orders/response-entity")
+    fun responseStatus(): ApiResponse<FindOrderResponse> {
+        return ApiResponse.success(FindOrderResponse(id = 1, memberName = "xxx", products = listOf()))
+    }
+
+    @PostMapping("/v1/orders/response-entity")
+    fun responseEntity(): ResponseEntity<ApiResponse<FindOrderResponse>> {
+        return ResponseEntity.status(HttpStatus.CREATED)
+            .body(ApiResponse.success(FindOrderResponse(id = 1, memberName = "xxx", products = listOf())))
+    }
+}
