cdfcf01faf364b1059c5b944f2e09411eb32725d
junho3 <chojunho433@gmail.com>
2024-06-05 10:44:12 +0900

    lint 수정

--- src/test/kotlin/com/example/demo/RestdocsUtils.kt
+++ src/test/kotlin/com/example/demo/RestdocsUtils.kt
@@ -8,21 +8,19 @@ import org.springframework.restdocs.payload.JsonFieldType.STRING
 import org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath
 
 object RestdocsUtils {
-    fun objectResponse(): Array<FieldDescriptor> {
-        return arrayOf(
+    fun objectResponse(): Array<FieldDescriptor> =
+        arrayOf(
             fieldWithPath("success").type(BOOLEAN).description("성공 여부"),
             fieldWithPath("code").type(STRING).description("실패 시 에러 코드").optional(),
             fieldWithPath("message").type(STRING).description("실패 시 메시지").optional(),
             fieldWithPath("data").type(OBJECT).description("응답 데이터").optional(),
         )
-    }
 
-    fun arrayResponse(): Array<FieldDescriptor> {
-        return arrayOf(
+    fun arrayResponse(): Array<FieldDescriptor> =
+        arrayOf(
             fieldWithPath("success").type(BOOLEAN).description("성공 여부"),
             fieldWithPath("code").type(STRING).description("실패 시 에러 코드").optional(),
             fieldWithPath("message").type(STRING).description("실패 시 메시지").optional(),
             fieldWithPath("data").type(ARRAY).description("응답 데이터").optional(),
         )
-    }
 }
