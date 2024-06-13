e079457057e2b9d9c7a8169f9c5fa36f8d8a1e8e
junho3 <chojunho433@gmail.com>
2024-06-13 11:47:09 +0900

    BigDecimal 테스트 케이스 추가

--- src/test/kotlin/com/example/etc/FloorTest.kt
+++ src/test/kotlin/com/example/etc/FloorTest.kt
@@ -3,6 +3,8 @@ package com.example.etc
 import io.kotest.core.annotation.DisplayName
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
+import java.math.BigDecimal
+import java.math.RoundingMode
 import kotlin.math.floor
 
 @DisplayName("양수, 음수에 따른 내림 테스트")
@@ -18,5 +20,29 @@ internal class FloorTest : FunSpec({
         val actual = floor(-4.5)
 
         actual shouldBe -5
+    }
+
+    test("BigDecimal 4.5를 setScale로 DOWN 하면 5가 된다.") {
+        val actual = BigDecimal("4.5").setScale(0, RoundingMode.DOWN)
+
+        actual shouldBe BigDecimal("4")
+    }
+
+    test("BigDecimal -4.5를 setScale로 DOWN 하면 -5가 된다.") {
+        val actual = BigDecimal("-4.5").setScale(0, RoundingMode.DOWN)
+
+        actual shouldBe BigDecimal("-4")
+    }
+
+    test("BigDecimal 4.5를 setScale로 FLOOR 하면 5가 된다.") {
+        val actual = BigDecimal("4.5").setScale(0, RoundingMode.FLOOR)
+
+        actual shouldBe BigDecimal("4")
+    }
+
+    test("BigDecimal -4.5를 setScale로 FLOOR 하면 -5가 된다.") {
+        val actual = BigDecimal("-4.5").setScale(0, RoundingMode.FLOOR)
+
+        actual shouldBe BigDecimal("-5")
     }
 })
