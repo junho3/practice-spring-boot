296e5d1b5b76904a874816e5424c7064beb08f40
Junho <chojunho433@gmail.com>
2025-01-02 21:55:07 +0900

    ProductTest 추가

---
+++ src/test/kotlin/com/example/demo/core/product/domain/ProductTest.kt
@@ -0,0 +1,33 @@
+package com.example.demo.core.product.domain
+
+import com.example.demo.FixturesMonkey
+import com.example.demo.PersistenceDataJpaTest
+import com.example.demo.core.member.domain.Member
+import com.example.demo.core.member.domain.Members
+import com.navercorp.fixturemonkey.kotlin.giveMeBuilder
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.annotation.DisplayName
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+import net.jqwik.api.Arbitraries
+import java.math.BigDecimal
+import kotlin.reflect.full.memberProperties
+
+@DisplayName(name = "Product")
+internal class ProductTest : FunSpec({
+
+    test("Product 엔티티는 name이 공백일 때 IllegalArgumentException을 던진다.") {
+        val name = ""
+        shouldThrow<IllegalArgumentException> { Product(name = name, price = BigDecimal.ONE) }
+    }
+
+    test("Product 엔티티는 price가 0미만 일 때 IllegalArgumentException을 던진다.") {
+        val price = FixturesMonkey
+            .fixture()
+            .giveMeBuilder<BigDecimal>()
+            .set(Arbitraries.bigDecimals().lessOrEqual(BigDecimal.ZERO))
+            .sample();
+
+        shouldThrow<IllegalArgumentException> { Product(name = "사과", price = price) }
+    }
+})
