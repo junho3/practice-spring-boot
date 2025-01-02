7f60b274b7d347e53e491380e6cd8f3deec416ce
Junho <chojunho433@gmail.com>
2025-01-02 22:23:29 +0900

    테스트 코드 추가

--- src/test/kotlin/com/example/demo/core/product/domain/ProductTest.kt
+++ src/test/kotlin/com/example/demo/core/product/domain/ProductTest.kt
@@ -30,4 +30,12 @@ internal class ProductTest : FunSpec({
 
         shouldThrow<IllegalArgumentException> { Product(name = "사과", price = price) }
     }
+
+    test("Product.name은 처음 세 글자가 '상품_'을 포함한다.") {
+        val name = "사과"
+
+        val actual = Product(name = name, price = BigDecimal.ONE)
+
+        actual.name shouldBe "상품_사과"
+    }
 })
